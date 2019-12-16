723546e6b549253cbe69f7306f228927648cb6d7
simurgh9 <simurgh9@protonmail.com>
2019-12-15 21:49:28 -0600

    fixed the repository name in package.json and rebuilt

---
+++ build/precache-manifest.aebc501a7cd731a29776d5531733b486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdcce1a8191f3bcbc9baf26025fc1a1",
+    "url": "/fractalsetc/build/index.html"
+  },
+  {
+    "revision": "137918ea45eac739b3bd",
+    "url": "/fractalsetc/build/static/css/main.cce9c4b3.chunk.css"
+  },
+  {
+    "revision": "a74a9507acb72839cf13",
+    "url": "/fractalsetc/build/static/js/2.2a3bdd68.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fractalsetc/build/static/js/2.2a3bdd68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "137918ea45eac739b3bd",
+    "url": "/fractalsetc/build/static/js/main.09b664be.chunk.js"
+  },
+  {
+    "revision": "f7dd4789e6aa2420461e",
+    "url": "/fractalsetc/build/static/js/runtime-main.b0ebe892.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fractaletc/build/precache-manifest.55182d47ac968b294252684d298e708a.js"
+  "/fractalsetc/build/precache-manifest.aebc501a7cd731a29776d5531733b486.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fractaletc/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fractalsetc/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
