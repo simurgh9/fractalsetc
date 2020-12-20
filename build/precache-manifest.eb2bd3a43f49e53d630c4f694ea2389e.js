afefafa83b4e194ed688a729085f0fde8894debd
Tashfeen <simurgh9@pm.me>
2020-12-20 02:19:16 -0600

    updated package.json

---
+++ build/precache-manifest.eb2bd3a43f49e53d630c4f694ea2389e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb8461b821f86cc1aa86327cdcde599",
+    "url": "/fractalsetc/build/index.html"
+  },
+  {
+    "revision": "e100330d576aacda2f0a",
+    "url": "/fractalsetc/build/static/css/main.1405cbcd.chunk.css"
+  },
+  {
+    "revision": "0cc9f2500a0c2bffe267",
+    "url": "/fractalsetc/build/static/js/2.699a244c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fractalsetc/build/static/js/2.699a244c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e100330d576aacda2f0a",
+    "url": "/fractalsetc/build/static/js/main.81682b44.chunk.js"
+  },
+  {
+    "revision": "c003bbfdd73e7b715acc",
+    "url": "/fractalsetc/build/static/js/runtime-main.091c690b.js"
+  }
+]);
