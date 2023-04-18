75ac9c8c3af527fe4dfba718aa4ea0ec5717e12d
smiurgh9 <simurgh9@pm.me>
2023-04-17 20:01:49 -0500

    updated according to the new root creating in react 18.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './view/css/index.css';
 import App from './view/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
